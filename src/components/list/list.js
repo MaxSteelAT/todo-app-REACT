5331e80f18a36dbea7e4cb6580b8ad79599bb620
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-04-21 15:08:19 -0700

    The logic was added to the List component.

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,45 +1,65 @@
 import './list.scss'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function List() {
+  const [list, setList]= useState([
+    {
+      done: true,
+      text: 'Complete online JavaScript course'
+    },
+    {
+      done: false,
+      text: 'Jog around the park 3x'
+    },
+    {
+      done: true,
+      text: '10 minutes meditation'
+    },
+    {
+      done: true,
+      text: 'Read for 1 hour'
+    },
+    {
+      done: true,
+      text: 'Pick up groceries'
+    },
+    {
+      done: true,
+      text: 'Complete Todo App on Frontend Mentor'
+    }
+  ])
+
+  const handleChange = (indexSelected) => {
+    const listUpdated = list.map((item, index)=>{
+      if(indexSelected == index){
+        return {
+          // ...item,
+          text: item.text,
+          done: item.done == true ? false : true,
+
+        }
+      }
+      return item
+    })
+
+    setList(listUpdated)
+  }
+
   return (
     <div className="todo-list">
       <div>
-<div> 
-        <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" checked />
-          <span>Complete online JavaScript course</span>
-        </label>
-       
-        <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" checked />
-          <span>Jog around the park 3x</span>
-        </label>
-
- <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" checked />
-          <span>10 minutes meditation</span>
-        </label>
-       
-        <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" />
-          <span>Read for 1 hour</span>
-        </label>
-
-        <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" checked />
-          <span>Pick up groceries</span>
-        </label>
-
-        <label className="input-label">
-          <input type="checkbox" className="todo-checkbox" checked />
-          <span>Complete Todo App on Frontend Mentor</span>
-        </label>
+        <div>
+          {list.map((item, index) => {
+            return (
+              <label className="input-label">
+                <input onChange={()=>handleChange(index)} type="checkbox" className="todo-checkbox" checked={item.done} />
+                <span>{item.text}</span>
+              </label>
+            )
+          })}
+        </div>
       </div>
-
-      </div>
-      
-    </div >
+    </div>
   );
 }
 
