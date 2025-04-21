b952c2a90a007c92e343eac46215434a82d08475
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-04-21 10:10:07 -0700

    Se agregaron cambios en el estilo del componente list

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,13 +1,46 @@
 import './list.scss'
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
-function App() {
- 
-
+function List() {
   return (
-    <div className="header">
-    </div>
+    <div className="todo-list">
+      <div>
+<div> 
+        <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" checked />
+          <span>Complete online JavaScript course</span>
+        </label>
+       
+        <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" checked />
+          <span>Jog around the park 3x</span>
+        </label>
+
+ <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" checked />
+          <span>10 minutes meditation</span>
+        </label>
+       
+        <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" />
+          <span>Read for 1 hour</span>
+        </label>
+
+        <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" checked />
+          <span>Pick up groceries</span>
+        </label>
+
+        <label className="input-label">
+          <input type="checkbox" className="todo-checkbox" checked />
+          <span>Complete Todo App on Frontend Mentor</span>
+        </label>
+      </div>
+
+      </div>
+      
+    </div >
   );
 }
 
-export default App;
+export default List;
