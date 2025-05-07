f874bd2a77b22ec46c5da19ef9078e805388fbf8
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-05-06 18:40:25 -0700

    Se agregó la lógica al darkmode.

--- src/components/darkMode/darkMode.js
+++ src/components/darkMode/darkMode.js
@@ -1,14 +1,28 @@
-import './darkMode.scss'
-import { useState, useEffect } from 'react'
+import './darkMode.scss';
+import iconMoon from '../../assets/images/icon-moon.svg';
+import iconSun from '../../assets/images/icon-sun.svg'
+import { useState, useEffect } from 'react';
 
-function App() {
- 
+function DarkMode() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
+  const changeMode = () => {
+    setIsDarkMode(isDarkMode === false)
+    //done: item.done === true ? false : true,
+  }
   return (
     <div className="darkMode">
-      <div className='darMode-icon'>🌞</div>
+      <div className="darkMode-icon">
+        <button onClick={changeMode}>
+        <img 
+          src={isDarkMode ? iconMoon : iconSun} 
+          alt={isDarkMode ? "icon moon" : "icon sun"} 
+          className="icon-moon icon-sun"
+        />
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default DarkMode;
