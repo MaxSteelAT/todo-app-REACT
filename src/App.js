b2f3bf9ddb1004472ee0417ec8028a460a642cf2
Max Steel <mtra.ingrid.lopez@gmail.com>
2025-05-15 13:49:25 -0700

    Se agregó el darkMode al filter.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 import { Provider } from "react-redux";
-import Header from './components/header/header'
-import DarkMode from './components/darkMode/darkMode'
-import Input from './components/input/input'
+import Header from "./components/header/header";
+import DarkMode from "./components/darkMode/darkMode";
+import Input from "./components/input/input";
 
-import List from './components/list/list'
-import Filter from './components/filter/filter'
-import DragDropText from './components/dragDropText/dragDropText'
+import List from "./components/list/list";
+import Filter from "./components/filter/filter";
+import DragDropText from "./components/dragDropText/dragDropText";
 import store from "./redux/store";
 
 function App() {
-
   return (
     <Provider store={store}>
-    <div className="App">
-      <div className="header-container">
-        <div className='todo-container'>
-          <Header />
+      <div className="App">
+        <div className="header-container">
+          <div className="todo-container">
+            <Header />
+          </div>
+          <div className="darkMode-container">
+            <DarkMode />
+          </div>
         </div>
-        <div className='darkMode-container'>
-        <DarkMode />
+        <div className="list-container">
+          <Input />
+          <List />
+          <Filter />
+          <DragDropText />
         </div>
       </div>
-      <div className="list-container">
-        <Input />
-        <List />
-        <div className='filter-container'>
-        <Filter />
-        </div>
-        <DragDropText/>
-      </div>
-    </div></Provider>
+    </Provider>
   );
 }
 
--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,18 +1,22 @@
-import './filter.scss'
+import "./filter.scss";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  
+  const darkMode = useSelector((state) => state.darkMode);
+
   return (
-    <div className="filter">
-       <div class="row">
-    <div class="col-6 col-md-2 colums">Items</div>
-    <div class="col-6 col-md-6 colums content-filter-options">
-      <div className='element-filter'>All</div>
-      <div className='element-filter'>Active</div>
-      <div className='element-filter'>Completed</div>
-    </div>
-    <div class="col-6 col-md-4 colums">Clear Completed</div>
-  </div>
+    <div className={`filter-container ${darkMode ? "dark" : ""}`}>
+      <div className="filter">
+        <div class="row">
+          <div class="col-6 col-md-2 colums">Items</div>
+          <div class="col-6 col-md-6 colums content-filter-options">
+            <div className="element-filter">All</div>
+            <div className="element-filter">Active</div>
+            <div className="element-filter">Completed</div>
+          </div>
+          <div class="col-6 col-md-4 colums">Clear Completed</div>
+        </div>
+      </div>
     </div>
   );
 }
